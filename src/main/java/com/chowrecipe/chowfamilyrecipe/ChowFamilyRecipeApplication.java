c2f14e87f3976e79eabe1132cd17ccea77a2092a
Jiwen <given209@terpmail.umd.edu>
2024-10-31 06:31:21 -0700

    initial commit

---
+++ src/main/java/com/chowrecipe/chowfamilyrecipe/ChowFamilyRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.chowrecipe.chowfamilyrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChowFamilyRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChowFamilyRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chowrecipe/chowfamilyrecipe/ChowFamilyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chowrecipe.chowfamilyrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChowFamilyRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
